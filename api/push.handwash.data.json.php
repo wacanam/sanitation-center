8f9d48543488658c454e428cdff207597e7d9934
wacanam <wacanam@gmail.com>
2020-05-20 19:01:08 +0800

    api push.json

--- api/push.handwash.data.json.php
+++ api/push.handwash.data.json.php
@@ -1,5 +1,18 @@
 <?php
+    require_once "../classes/classes.db.php";
 
     $post = file_get_contents('php://input');
-    echo $post;
-    echo "visited";
+    $json = json_decode($post);
+    var_dump($json);
+
+    function updateBooth($data, $conn){
+        $sql = "UPDATE booth SET battery1_lvl = $data->battery1, battery2_lvl = $data->battery2, soap_lvl = $data->soap, water_lvl = $data->water WHERE id = 1;";
+        if ($conn->query($sql) === TRUE) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        } 
+        $conn->close();
+    }
+
+    updateBooth($json, $db_conn);
