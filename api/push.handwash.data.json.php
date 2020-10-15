83aa8b6a38f9592012e82fd804b79be8a8917171
Wave Cantery Ambray <wacanam@gmail.com>
2020-10-15 13:39:08 +0800

    remove battery 2

--- api/push.handwash.data.json.php
+++ api/push.handwash.data.json.php
@@ -1,19 +1,11 @@
 <?php
     require_once "../classes/classes.db.php";
 
-    // $post = file_get_contents('php://input');
-    
-    $post = array(
-        booth_id => 0,
-        battery1 => 78,
-        battery2 => 0,
-        soap => 53,
-        water => 84
-    );
+    $post = file_get_contents('php://input');
     $json = json_decode($post);
     var_dump($json);
     if($json){
-        updateBooth($post, $db_conn);
+        updateBooth($json, $db_conn);
     }
     function updateBooth($data, $conn){
         $sql = "UPDATE booth SET battery1_lvl = $data->battery1, soap_lvl = $data->soap, water_lvl = $data->water WHERE id = $data->booth_id;";
