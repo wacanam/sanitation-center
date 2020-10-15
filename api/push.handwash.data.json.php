74d71c3989037f8075ede19a92d77ba3bc7e828b
Wave Cantery Ambray <wacanam@gmail.com>
2020-10-15 13:05:56 +0800

    force push data

--- api/push.handwash.data.json.php
+++ api/push.handwash.data.json.php
@@ -1,23 +1,22 @@
 <?php
     require_once "../classes/classes.db.php";
 
-    //$post = file_get_contents('php://input');
-    //$json = json_decode($post);
-    //var_dump($json);
-    //if($json){
-        //updateBooth($json, $db_conn);
-    //}
-    $data = array(
+    // $post = file_get_contents('php://input');
+    
+    $post = array(
         "booth_id" => 0,
         "battery1" => 78,
         "battery2" => 0,
         "soap" => 53,
         "water" => 84
     );
-    updateBooth(json_encode($data), $db_conn);
-    
+    $json = json_encode($post);
+    var_dump($json);
+    if($json){
+        updateBooth($json, $db_conn);
+    }
     function updateBooth($data, $conn){
-        $sql = "UPDATE booth SET battery1_lvl = '$data->battery1', soap_lvl = '$data->soap', water_lvl = '$data->water' WHERE id = '$data->booth_id';";
+        $sql = "UPDATE booth SET battery1_lvl = $data->battery1, soap_lvl = $data->soap, water_lvl = $data->water WHERE id = $data->booth_id;";
         if ($conn->query($sql) === TRUE) {
             echo "Record updated successfully";
         } else {
